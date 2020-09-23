5f48c2fe8c2da862f7a6eae3e428469c85c24b5e
Narciso Junior <boldrin.narciso@gmail.com>
2020-09-23 16:05:10 -0300

    Remoção de arquivos desnecessários

--- app/Repositories/Contracts/UserRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Repositories\Contracts;
-
-interface UserRepositoryInterface
-{
-    public function getBalanceAttribute(User $user);
-    public function setBalanceAttribute($amount);
-}
